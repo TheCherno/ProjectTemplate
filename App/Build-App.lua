db2fe9e0c52b5cd096316f6be4ba0510ba3f003a
Collie_ <99091783+Collie1@users.noreply.github.com>
2023-12-28 11:14:20 +0100

    fixed indetation in Build-App.lua

--- App/Build-App.lua
+++ App/Build-App.lua
@@ -1,45 +1,45 @@
 project "App"
-   kind "ConsoleApp"
-   language "C++"
-   cppdialect "C++20"
-   targetdir "Binaries/%{cfg.buildcfg}"
-   staticruntime "off"
+   	kind "ConsoleApp"
+   	language "C++"
+   	cppdialect "C++20"
+   	targetdir "Binaries/%{cfg.buildcfg}"
+  	staticruntime "off"
 
-   files { "Source/**.h", "Source/**.cpp" }
+   	files { "Source/**.h", "Source/**.cpp" }
 
-   includedirs
-   {
-      "Source",
+   	includedirs
+   	{
+      	"Source",
 
-	  -- Include Core
-	  "../Core/Source"
-   }
+	 	 -- Include Core
+	 	"../Core/Source"
+   	}
 
-   links
-   {
-      "Core"
-   }
+    links
+    {
+    	"Core"
+    }
 
-   targetdir ("../Binaries/" .. OutputDir .. "/%{prj.name}")
-   objdir ("../Binaries/Intermediates/" .. OutputDir .. "/%{prj.name}")
+    targetdir ("../Binaries/" .. OutputDir .. "/%{prj.name}")
+    objdir ("../Binaries/Intermediates/" .. OutputDir .. "/%{prj.name}")
 
-   filter "system:windows"
-       systemversion "latest"
-       defines { "WINDOWS" }
+    filter "system:windows"
+        systemversion "latest"
+        defines { "WINDOWS" }
 
-   filter "configurations:Debug"
-       defines { "DEBUG" }
-       runtime "Debug"
-       symbols "On"
+    filter "configurations:Debug"
+        defines { "DEBUG" }
+        runtime "Debug"
+        symbols "On"
 
-   filter "configurations:Release"
-       defines { "RELEASE" }
-       runtime "Release"
-       optimize "On"
-       symbols "On"
+    filter "configurations:Release"
+        defines { "RELEASE" }
+        runtime "Release"
+        optimize "On"
+        symbols "On"
 
-   filter "configurations:Dist"
-       defines { "DIST" }
-       runtime "Release"
-       optimize "On"
-       symbols "Off"
+    filter "configurations:Dist"
+        defines { "DIST" }
+        runtime "Release"
+        optimize "On"
+        symbols "Off"

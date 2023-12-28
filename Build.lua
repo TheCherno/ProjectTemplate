32996428c26cc66b666e18c44d0a66ac3476a220
Collie_ <99091783+Collie1@users.noreply.github.com>
2023-12-28 11:11:53 +0100

    Fixed indentation in Build.lua

--- Build.lua
+++ Build.lua
@@ -1,12 +1,12 @@
 -- premake5.lua
 workspace "New Project"
-   architecture "x64"
-   configurations { "Debug", "Release", "Dist" }
-   startproject "App"
+	architecture "x64"
+   	configurations { "Debug", "Release", "Dist" }
+   	startproject "App"
 
-   -- Workspace-wide build options for MSVC
-   filter "system:windows"
-      buildoptions { "/EHsc", "/Zc:preprocessor", "/Zc:__cplusplus" }
+   	-- Workspace-wide build options for MSVC
+   	filter "system:windows"
+      	buildoptions { "/EHsc", "/Zc:preprocessor", "/Zc:__cplusplus" }
 
 OutputDir = "%{cfg.system}-%{cfg.architecture}/%{cfg.buildcfg}"
 
